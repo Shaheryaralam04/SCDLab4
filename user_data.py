b53fdefe1f7118b710de4d86e84b9bf2b37d6b26
Shaheryaralam04 <146157859+Shaheryaralam04@users.noreply.github.com>
2023-10-18 10:33:54 +0500

    Delete user_data.py

--- user_data.py
+++
@@ -1,9 +0,0 @@
-while True:
-    data = input("Enter data (or 'q' to quit): ")
-    if data.lower() == 'q':
-        break
-    user_data.append(data)
-
-print("User Data:")
-for i, data in enumerate(user_data, 1):
-    print(f"{i}. {data}")
